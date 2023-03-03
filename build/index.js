5ffd11a0e92b3430190022bf7bb6b646a2709425
nika <nikacuckiridze47@gmail.com>
2023-03-03 23:01:21 +0400

    Initial commit: Basic environment setup

---
+++ build/index.js
@@ -0,0 +1,4 @@
+var t1 = performance.now();
+console.log();
+var t2 = performance.now();
+console.log("takes ".concat((t2 - t1) / 1000, " seconds"));
---
+++ index.ts
@@ -0,0 +1,5 @@
+const t1 = performance.now();
+console.log();
+const t2 = performance.now();
+
+console.log(`takes ${(t2 - t1) / 1000} seconds`);
