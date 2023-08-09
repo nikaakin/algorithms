32a44da371ce35edf1053fc7ed2d30de2d0c2a82
Nika Tsutskiridze <nikacuckiridze47@gmail.com>
2023-08-09 14:39:54 +0400

    refactor: add absolute paths

--- index.ts
+++ index.ts
@@ -1,8 +1,8 @@
-import { SinglyLinkedList } from "./version-0/linked-list/LinkedList";
+import { SinglyLinkedList } from "@/version-0/linked-list/LinkedList";
 
 const t1 = performance.now();
 console.log(
-  new SinglyLinkedList().unshift(10).push(2).push(3).push(5).reverse()
+  new SinglyLinkedList().unshift(10).push(2).push(3).push(3).reverse()
 );
 const t2 = performance.now();
 
