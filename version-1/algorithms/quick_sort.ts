0a8abbef74406538d7cb0eae99fc10a5441a81b2
Nika Cuckiridze <nikacuckiridze47@gmail.com>
2023-08-31 13:41:27 +0400

    fix: quick sort

--- version-1/algorithms/quick_sort.ts
+++ version-1/algorithms/quick_sort.ts
@@ -1,17 +1,44 @@
-export const quick_sort = (arr: number[]): number[] => {
-  if (arr.length <= 1) return arr;
-  let pivot = 0;
+export const quick_sort = (
+  arr: number[],
+  low = 0,
+  high = arr.length - 1
+): number[] => {
+  if (low >= high) return;
+  let pivot = Math.floor(low + (high - low) / 2);
 
-  for (let i = 1; i < arr.length; i++) {
-    if (arr[pivot] > arr[i]) {
-      let temp = arr[pivot];
+  console.log(low, high);
+
+  for (let i = low; i <= high; i++) {
+    console.log(pivot, arr);
+    if (pivot === i) continue;
+
+    if (pivot < i && arr[pivot] >= arr[i]) {
+      const temp = arr[pivot];
       arr[pivot] = arr[i];
-      arr[i] = temp;
-      pivot = i;
+      if (pivot + 1 === i) {
+        arr[i] = temp;
+      } else {
+        const temp1 = arr[pivot + 1];
+        arr[pivot + 1] = temp;
+        arr[i] = temp1;
+      }
+      pivot++;
+    } else if (pivot > i && arr[pivot] <= arr[i]) {
+      const temp = arr[pivot];
+      arr[pivot] = arr[i];
+      if (pivot - 1 === i) {
+        arr[i] = temp;
+      } else {
+        const temp1 = arr[pivot - 1];
+        arr[pivot - 1] = temp;
+        arr[i] = temp1;
+      }
+      pivot--;
     }
   }
+  console.log(arr, pivot);
 
-  return quick_sort(arr.slice(0, pivot + 1)).concat(
-    quick_sort(arr.slice(pivot + 1))
-  );
+  quick_sort(arr, low, pivot - 1);
+  quick_sort(arr, pivot + 1, high);
+  return arr;
 };
